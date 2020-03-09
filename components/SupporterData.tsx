60562a6c5673870d79408d894099d85e4d90899e
Bob Ippolito <bob@redivi.com>
2020-03-09 15:44:08 -0700

    Update next.js

---
+++ components/SupporterData.tsx
@@ -0,0 +1,64 @@
+export interface SupporterDataProps {
+  name: string;
+  href: string;
+  title: string;
+}
+
+const SupporterData: SupporterDataProps[] = [
+  { name: "anthem", title: "Anthem", href: "https://www.antheminc.com/" },
+  {
+    name: "sfbfa",
+    title: "SF Black Firefighters Association",
+    href: "https://www.sfbfa.org/"
+  },
+  { name: "ccpa", title: "CCPA", href: "https://www.ousd.org/ccpa" },
+  { name: "ccsf", title: "CCSF", href: "https://www.ccsf.edu/" },
+  { name: "clearbit", title: "Clearbit", href: "https://clearbit.com/" },
+  { name: "facebook", title: "Facebook", href: "https://www.facebook.com/" },
+  {
+    name: "ghs",
+    title: "Gateway",
+    href: "https://www.gatewaypublicschools.org/ghs"
+  },
+  { name: "giants", title: "Giants", href: "https://www.mlb.com/giants" },
+  {
+    name: "warriors",
+    title: "Warriors",
+    href: "https://www.nba.com/warriors/"
+  },
+  {
+    name: "hackreactor",
+    title: "Hack Reactor",
+    href: "https://www.hackreactor.com/"
+  },
+  { name: "jyve", title: "Jyve", href: "https://jyve.com/" },
+  {
+    name: "kapor",
+    title: "Kapor Center",
+    href: "https://www.kaporcenter.org/"
+  },
+  {
+    name: "leadership",
+    title: "Leadership High",
+    href: "https://www.leadershiphigh.org/"
+  },
+  {
+    name: "ousd",
+    title: "Oakland Unified School District",
+    href: "https://www.ousd.org/"
+  },
+  {
+    name: "shorenstein",
+    title: "Shorenstein",
+    href: "https://shorenstein.com/"
+  },
+  { name: "svb", title: "Silicon Valley Bank", href: "https://www.svb.com/" },
+  { name: "oewd", title: "OEWD", href: "https://oewd.org/" },
+  { name: "sfusd", title: "SFUSD", href: "https://www.sfusd.edu/" },
+  { name: "tng", title: "TNG", href: "https://www.tng.com/" },
+  { name: "uber", title: "Uber", href: "https://www.uber.com/" },
+  { name: "unity", title: "Unity", href: "https://unity3d.com/" },
+  { name: "eastwood", title: "Eastwood", href: "https://eastwoodsf.com/" }
+];
+
+export default SupporterData;
