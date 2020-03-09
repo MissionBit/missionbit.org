1c2784506b4a078ce3aa590ffab6fd480933970d
Bob Ippolito <bob@redivi.com>
2020-03-09 14:32:02 -0700

    consolidate css to makeStyles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,27 @@ import * as React from "react";
 import Layout from "../components/Layout";
 import Landing from "../components/index/Landing";
 import Index from "../components/index";
-import styles from "../styles/index.module.css";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Page: NextPage<{}> = () => (
-  <Layout
-    title="Mission Bit"
-    headerChildren={<Landing />}
-    headerClassName={styles.header}
-  >
-    <Index />
-  </Layout>
-);
+const useStyles = makeStyles(() => ({
+  header: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh"
+  }
+}));
+
+const Page: NextPage<{}> = () => {
+  const classes = useStyles();
+  return (
+    <Layout
+      title="Mission Bit"
+      headerChildren={<Landing />}
+      headerClassName={classes.header}
+    >
+      <Index />
+    </Layout>
+  );
+};
 
 export default Page;
