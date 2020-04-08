07b465d17a36fd9d502760691e630b388393532f
Bob Ippolito <bob@redivi.com>
2020-04-07 18:07:50 -0700

    Add a 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: "center",
+    padding: theme.spacing(4, 0),
+    "& h1": {
+      margin: theme.spacing(4, 0),
+    },
+  },
+}));
+
+const Page: NextPage<{}> = () => (
+  <Layout title="Mission Bit – 404 Not Found">
+    <Container component="main" className={useStyles().root}>
+      <Typography variant="h1">HTTP 404 Not Found</Typography>
+      <Typography variant="h3">
+        <Link href="/">Check out our homepage</Link>
+      </Typography>
+    </Container>
+  </Layout>
+);
+
+export default Page;
