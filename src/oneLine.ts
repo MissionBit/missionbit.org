ecaaa7cd2218489a9108e5e721b505872ff6bb78
Bob Ippolito <bob@redivi.com>
2020-04-19 16:21:17 -0700

    add oneLine

---
+++ src/oneLine.ts
@@ -0,0 +1,12 @@
+function oneLine(strs: TemplateStringsArray, ...placeholders: unknown[]) {
+  // Build the string as normal, combining all the strings and placeholders:
+  let result = "";
+  let i = 0;
+  for (const str of strs) {
+    result += str + (placeholders[i] ?? "");
+    i += 1;
+  }
+  return result.trim().replace(/\s\s+/g, " ");
+}
+
+export default oneLine;
