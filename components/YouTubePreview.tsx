3be208b93b737513143f9c88940648b1410a96bc
Bob Ippolito <bob@redivi.com>
2020-04-21 23:30:23 -0700

    Presize youtube previews

--- components/YouTubePreview.tsx
+++ components/YouTubePreview.tsx
@@ -6,50 +6,58 @@ import Link from "@material-ui/core/Link";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "inline-flex",
-    flexDirection: "column",
-    padding: theme.spacing(2),
-    width: 300,
-    alignItems: "center",
-    "&:hover $overlay": {
-      opacity: "100%",
-      "&$youTube": {
-        color: "red",
-      },
-      "&$playArrow": {
-        color: theme.palette.common.white,
+const useStyles = makeStyles((theme) => {
+  const width = 300;
+  const padding = theme.spacing(2);
+  const imgWidth = 300 - 2 * padding;
+  const imgHeight = (720 / 1280) * imgWidth;
+
+  return {
+    root: {
+      display: "inline-flex",
+      flexDirection: "column",
+      padding,
+      width,
+      alignItems: "center",
+      "&:hover $overlay": {
+        opacity: "100%",
+        "&$youTube": {
+          color: "red",
+        },
+        "&$playArrow": {
+          color: theme.palette.common.white,
+        },
       },
     },
-  },
-  title: {
-    marginTop: theme.spacing(2),
-  },
-  img: {
-    maxWidth: "100%",
-    height: "auto",
-  },
-  overlay: {
-    position: "absolute",
-    left: "50%",
-    top: "50%",
-    transform: "translate(-50%, -50%)",
-    opacity: "80%",
-    transition: theme.transitions.easing.easeIn,
-  },
-  youTube: {
-    fontSize: "3rem",
-    color: theme.palette.common.white,
-  },
-  playArrow: {
-    fontSize: "1.455rem",
-    color: theme.palette.common.black,
-  },
-  wrapper: {
-    position: "relative",
-  },
-}));
+    title: {
+      marginTop: theme.spacing(2),
+    },
+    img: {
+      width: imgWidth,
+      height: imgHeight,
+      maxWidth: "100%",
+    },
+    overlay: {
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      transform: "translate(-50%, -50%)",
+      opacity: "80%",
+      transition: theme.transitions.easing.easeIn,
+    },
+    youTube: {
+      fontSize: "3rem",
+      color: theme.palette.common.white,
+    },
+    playArrow: {
+      fontSize: "1.455rem",
+      color: theme.palette.common.black,
+    },
+    wrapper: {
+      position: "relative",
+    },
+  };
+});
 
 const YouTubePreview: React.FC<{ id: string }> = ({ id, children }) => {
   const classes = useStyles();
