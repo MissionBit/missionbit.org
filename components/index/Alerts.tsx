c77ae63e2cb9a48971595d651a35afff56b796d2
Bob Ippolito <bob@redivi.com>
2020-04-22 15:03:05 -0700

    Generalize alerts and make orange

---
+++ components/index/Alerts.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import Link from "@material-ui/core/Link";
+import Alert from "@material-ui/lab/Alert";
+import Collapse from "@material-ui/core/Collapse";
+import { useEffect, useState } from "react";
+import { SummerDates } from "../programs/ClassInstanceData";
+import { ShortDateFormat } from "../../src/dates";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flex: "1 0 auto",
+    '& div[role="alert"]': {
+      borderRadius: 0,
+      "& .MuiAlert-icon": {
+        display: "none",
+      },
+      "& .MuiAlert-message": {
+        width: "100%",
+        fontWeight: "bold",
+        textAlign: "center",
+      },
+    },
+  },
+}));
+
+interface TimedAlert {
+  hideBefore?: number;
+  hideAfter?: number;
+  content: React.ReactNode;
+}
+
+const timedAlerts: TimedAlert[] = [
+  {
+    hideAfter: SummerDates.registrationDeadline,
+    content: (
+      <>
+        <Link color="inherit" underline="always" href="/programs">
+          Sign up
+        </Link>{" "}
+        for our Online Summer 2020 Courses now! Deadline{" "}
+        {ShortDateFormat.format(SummerDates.registrationDeadline)}
+      </>
+    ),
+  },
+];
+
+function firstAlert(now: number): React.ReactNode | undefined {
+  return timedAlerts.find(
+    ({ hideBefore, hideAfter }) =>
+      (hideBefore === undefined || now >= hideBefore) &&
+      (hideAfter === undefined || now <= hideAfter)
+  )?.content;
+}
+
+const firstAlertNow = () => firstAlert(Date.now());
+
+const Alerts: React.FC<{}> = () => {
+  const classes = useStyles();
+  const [alert, setAlert] = useState<React.ReactNode | undefined>(
+    firstAlertNow
+  );
+  useEffect(() => setAlert(firstAlertNow()), []);
+  return (
+    <Collapse in={alert !== undefined} className={classes.root}>
+      <Alert
+        severity="info"
+        variant="filled"
+        color="warning"
+        onClose={() => setAlert(undefined)}
+      >
+        {alert}
+      </Alert>
+    </Collapse>
+  );
+};
+
+export default Alerts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import { NextPage } from "next";
 import * as React from "react";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Link from "@material-ui/core/Link";
-import Alert from "@material-ui/lab/Alert";
-import Collapse from "@material-ui/core/Collapse";
 import Layout from "../components/Layout";
 import Landing from "../components/index/Landing";
 import Index from "../components/index";
-import { SummerDates } from "../components/programs/ClassInstanceData";
-import { ShortDateFormat } from "../src/dates";
+import Alerts from "../components/index/Alerts";
 
 const useStyles = makeStyles(() => ({
   header: {
@@ -18,31 +14,7 @@ const useStyles = makeStyles(() => ({
     height: "100vh",
     maxHeight: "var(--document-height, 100vh)",
   },
-  alerts: {
-    flex: "1 0 auto",
-    '& div[role="alert"]': {
-      alignItems: "center",
-    },
-  },
 }));
-
-const Alerts: React.FC<{ className?: string }> = ({ className }) => {
-  const deadline = SummerDates.registrationDeadline;
-  const isValid = useCallback(() => deadline - Date.now() > 0, [deadline]);
-  const [open, setOpen] = useState(isValid);
-  useEffect(() => setOpen(isValid()), [isValid]);
-  return (
-    <Collapse in={open} className={className}>
-      <Alert severity="info" onClose={() => setOpen(false)}>
-        <Link color="inherit" underline="always" href="/programs">
-          Sign up
-        </Link>{" "}
-        for our Online Summer 2020 Courses now! Deadline{" "}
-        {ShortDateFormat.format(deadline)}
-      </Alert>
-    </Collapse>
-  );
-};
 
 function updateDocumentHeight() {
   if (typeof document === "undefined") {
@@ -62,7 +34,7 @@ const Page: NextPage<{}> = () => {
   return (
     <Layout
       title="Mission Bit"
-      alerts={<Alerts className={classes.alerts} />}
+      alerts={<Alerts />}
       headerChildren={<Landing />}
       headerClassName={classes.header}
     >
