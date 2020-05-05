e48a63bef42882b651108266f369538ee5b56673
Bob Ippolito <bob@redivi.com>
2020-05-04 23:38:13 -0700

    Enable source maps

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
+const withSourceMaps = require("@zeit/next-source-maps");
 
 const slashRedirects = [
   "about",
@@ -58,34 +59,37 @@ const nextConfig = {
 
 module.exports = withPlugins(
   [
-    optimizedImages,
-    {
-      // these are the default values so you don't have to provide them if they are good enough for your use-case.
-      // but you can overwrite them here with any valid value you want.
-      inlineImageLimit: 8192,
-      imagesFolder: "images",
-      imagesName: "[name]-[hash].[ext]",
-      handleImages: ["jpeg", "png", "svg", "webp", "gif"],
-      optimizeImages: true,
-      optimizeImagesInDev: false,
-      responsive: {},
-      mozjpeg: {
-        quality: 80,
+    [withSourceMaps],
+    [
+      optimizedImages,
+      {
+        // these are the default values so you don't have to provide them if they are good enough for your use-case.
+        // but you can overwrite them here with any valid value you want.
+        inlineImageLimit: 8192,
+        imagesFolder: "images",
+        imagesName: "[name]-[hash].[ext]",
+        handleImages: ["jpeg", "png", "svg", "webp", "gif"],
+        optimizeImages: true,
+        optimizeImagesInDev: false,
+        responsive: {},
+        mozjpeg: {
+          quality: 80,
+        },
+        optipng: {
+          optimizationLevel: 3,
+        },
+        pngquant: false,
+        // Disabled gifsicle, does not compile in zeit environment and we do not use gifs
+        gifsicle: false,
+        svgo: {
+          // enable/disable svgo plugins here
+        },
+        webp: {
+          preset: "default",
+          quality: 75,
+        },
       },
-      optipng: {
-        optimizationLevel: 3,
-      },
-      pngquant: false,
-      // Disabled gifsicle, does not compile in zeit environment and we do not use gifs
-      gifsicle: false,
-      svgo: {
-        // enable/disable svgo plugins here
-      },
-      webp: {
-        preset: "default",
-        quality: 75,
-      },
-    },
+    ],
   ],
   nextConfig
 );
