37258c7a691fc12f3cecd3830bd9c7d57b5a24c4
Bob Ippolito <bob@redivi.com>
2020-05-08 22:36:33 -0700

    ExpandMore

--- components/FaqItem.tsx
+++ components/FaqItem.tsx
@@ -3,7 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandMoreIcon from "./icons/ExpandMore";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
---
+++ components/icons/ExpandMore.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import SvgIcon, { SvgIconProps } from "@material-ui/core/SvgIcon";
+
+const ExpandMoreIcon = (props: SvgIconProps) => (
+  <SvgIcon viewBox="-50 -50 100 100" {...props}>
+    <polyline
+      stroke="currentColor"
+      fill="none"
+      strokeWidth="18%"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      points="-40,-18 0,18 40,-18"
+    />
+  </SvgIcon>
+);
+
+export default ExpandMoreIcon;
