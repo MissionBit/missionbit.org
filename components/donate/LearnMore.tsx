35f172bb4c5f70e4b33383126c30e4a763ac44ae
Bob Ippolito <bob@redivi.com>
2020-06-14 10:03:12 -0700

    WIP faq

--- components/donate/LearnMore.tsx
+++ components/donate/LearnMore.tsx
@@ -1,13 +1,30 @@
 import * as React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import clsx from "clsx";
+import IndigoButton from "components/IndigoButton";
+import AdobePdfLogo from "components/gala/brands/AdobePdfLogo";
+import { ExtendButtonBase } from "@material-ui/core/ButtonBase";
+import { ButtonTypeMap } from "@material-ui/core/Button";
+
+const LearnMoreButton = withStyles((theme) => ({
+  contained: {
+    borderRadius: 0,
+    padding: theme.spacing(3),
+    ...theme.typography.h5,
+    "& svg": {
+      marginRight: "0.25rem",
+    },
+  },
+}))(IndigoButton) as ExtendButtonBase<ButtonTypeMap>;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    border: "1px solid black",
-    padding: theme.spacing(2),
+    padding: theme.spacing(0, 2),
+    "& > *:nth-child(n + 2)": {
+      marginTop: theme.spacing(2),
+    },
   },
 }));
 
@@ -16,6 +33,17 @@ export const LearnMore: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <Box component="section" className={clsx(classes.root, className)}>
       <Typography variant="h2">Learn More</Typography>
+      <Typography variant="body1">
+        Check out our Annual Reports to learn more about our work and impact
+      </Typography>
+      <LearnMoreButton
+        variant="contained"
+        href="/annual-reports/2018/mission-bit-annual-report-2018.pdf"
+        target="_blank"
+        size="large"
+      >
+        <AdobePdfLogo fontSize="small" /> 2018 Annual Report
+      </LearnMoreButton>
     </Box>
   );
 };
--- components/donate/OtherWaysToGive.tsx
+++ components/donate/OtherWaysToGive.tsx
@@ -1,13 +1,54 @@
 import * as React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
 import clsx from "clsx";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import FaqItem from "components/FaqItem";
+import { brand } from "src/colors";
+import BaseLink from "@material-ui/core/Link";
+
+const accentColor = brand.indigo;
+
+const Link = withStyles((theme) => ({
+  root: {
+    color: theme.palette.common.white,
+  },
+}))(BaseLink);
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    border: "1px solid black",
-    padding: theme.spacing(2),
+    "& .MuiPaper-root": {
+      backgroundColor: brand.indigo,
+      color: theme.palette.common.white,
+    },
+    "& .MuiExpansionPanel-root::before": {
+      backgroundColor: theme.palette.common.white,
+    },
+    "& .MuiExpansionPanelSummary-expandIcon": {
+      color: theme.palette.common.white,
+      "& .MuiSvgIcon-root": {
+        fontSize: theme.typography.pxToRem(48),
+        [theme.breakpoints.down("sm")]: {
+          fontSize: theme.typography.pxToRem(28),
+        },
+      },
+    },
+    "& .MuiPaper-elevation1": {
+      boxShadow: `0px 2px 1px -1px ${accentColor}, 0px 1px 1px 0px ${accentColor}, 0px 1px 3px 0px ${accentColor}`,
+    },
+    [theme.breakpoints.down("xs")]: {
+      paddingLeft: 0,
+      paddingRight: 0,
+    },
+  },
+  title: {
+    [theme.breakpoints.down("sm")]: {
+      fontSize: theme.typography.h5.fontSize,
+    },
+  },
+  address: {
+    marginTop: theme.spacing(2),
+    fontStyle: "normal",
   },
 }));
 
@@ -16,9 +57,82 @@ export const OtherWaysToGive: React.FC<{ className?: string }> = ({
 }) => {
   const classes = useStyles();
   return (
-    <Box component="section" className={clsx(classes.root, className)}>
-      <Typography variant="h2">Other Ways to Give</Typography>
-    </Box>
+    <Container
+      component="section"
+      id="other-ways-to-give"
+      className={clsx(classes.root, className)}
+    >
+      <Typography variant="h2" className={classes.title}>
+        Other Ways To Give
+      </Typography>
+      <FaqItem question="Check">
+        <Typography>
+          Make checks payable to <strong>Mission Bit</strong>. To receive a
+          receipt electronically, write your email address in the memo of your
+          check.
+        </Typography>
+        <Typography component="address" className={classes.address}>
+          Mission Bit
+          <br />
+          101 A Clay Street Ste. 121
+          <br />
+          San Francisco, CA 94111
+          <br />
+          <br />
+          EIN: 46-0945785
+          <br />
+          Phone: (415) 879-5380
+          <br />
+          Contact: info@missionbit.org
+          <br />
+          Mission Bit is a 501 (c)(3)
+        </Typography>
+      </FaqItem>
+      <FaqItem question="Online">
+        Online donations are the easiest way to give to Mission Bit. Please
+        visit our secure online form above to make your donation today, and
+        consider making your gift a monthly commitment to provide computer
+        science education for free by choosing “Monthly”.
+      </FaqItem>
+      <FaqItem question="Matching Gift">
+        You may be able to double or triple your gift through your employer’s
+        matching gift program. Contact your HR department to find out if your
+        company matches your charitable donations. If they do, let us know!
+      </FaqItem>
+      <FaqItem question="In-Kind Gift">
+        We accept in-kind donations of laptops and other equipment.{" "}
+        <Link
+          href="mailto:info@missionbit.org"
+          target="_blank"
+          underline="always"
+          rel="noopener noreferrer"
+        >
+          Contact Us
+        </Link>{" "}
+        if you have something to donate.
+      </FaqItem>
+      <FaqItem question="Anything else?">
+        For more information or assistance, please contact us by email at{" "}
+        <Link
+          href="mailto:development@missionbit.org"
+          target="_blank"
+          underline="always"
+          rel="noopener noreferrer"
+        >
+          development@missionbit.org
+        </Link>{" "}
+        or by phone at{" "}
+        <Link
+          href="tel:+14158795380"
+          target="_blank"
+          underline="always"
+          rel="noopener noreferrer"
+        >
+          (415) 879-5380
+        </Link>
+        .
+      </FaqItem>
+    </Container>
   );
 };
 
