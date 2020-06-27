a650d95d105ca82671a38d5ffc80ac258f34ca90
Bob Ippolito <bob@redivi.com>
2020-06-27 10:43:57 -0700

    Start on donation receipt

--- components/donate/DonateResult.tsx
+++ components/donate/DonateResult.tsx
@@ -1,16 +1,36 @@
 import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Landing from "./Landing";
+import Box from "@material-ui/core/Box";
 import { StripeSessionInfo } from "src/stripeSessionInfo";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 import usdFormatter from "src/usdFormatter";
 import { DONATE_EMAIL } from "src/emails";
+import ReceiptPhotos from "./ReceiptPhotos";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    display: "grid",
+    gridTemplateAreas: `
+      "photos receipt"
+    `,
+    gridTemplateColumns: "1fr 1fr",
     padding: theme.spacing(2),
+  },
+  receipt: {
+    gridArea: "receipt",
+    padding: theme.spacing(2),
+  },
+  photos: {
+    gridArea: "photos",
+  },
+  heading: {
+    textTransform: "uppercase",
+  },
+  subHeading: {
+    marginTop: theme.spacing(4),
+    textTransform: "uppercase",
   },
 }));
 
@@ -18,12 +38,32 @@ const DonateResult: React.FC<{ sessionInfo: StripeSessionInfo }> = ({
   sessionInfo,
 }) => {
   const classes = useStyles();
-  const { amount, email, frequency, payment_method, id } = sessionInfo;
+  const { amount, name, email, frequency, payment_method, id } = sessionInfo;
   return (
-    <main id="main">
-      <Landing />
-      <Container className={classes.root}>
-        <Typography variant="h2">Thank you!</Typography>
+    <Container component="main" id="main" className={classes.root}>
+      <ReceiptPhotos className={classes.photos} />
+      <Box className={classes.receipt}>
+        <Typography variant="h1" className={classes.heading}>
+          Thank you
+        </Typography>
+        <Typography>
+          Dear {name},<br />
+          <br />
+          We want to thank you for your generosity! You are a true champion of
+          this work. We look forward to our continued growth and success here at
+          Mission Bit.
+          <br />
+          <br />
+          Best,
+          <br />
+          <br />
+          Christina Ortega
+          <br />
+          CEO, Mission Bit
+        </Typography>
+        <Typography variant="h2" className={classes.subHeading}>
+          Donation Receipt
+        </Typography>
         <Typography>
           Your <strong>{usdFormatter.format(amount / 100)}</strong> {frequency}{" "}
           donation by {payment_method} has been collected and a receipt has been
@@ -51,8 +91,8 @@ const DonateResult: React.FC<{ sessionInfo: StripeSessionInfo }> = ({
         <Typography>
           <Link href="/donate">Back to the donate page</Link>
         </Typography>
-      </Container>
-    </main>
+      </Box>
+    </Container>
   );
 };
 
