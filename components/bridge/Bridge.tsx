9226c249ef59ec50af2e2f1c74d99fc26cc30f93
Bob Ippolito <bob@redivi.com>
2020-07-11 17:43:49 -0700

    Start adding style

--- components/bridge/Bridge.tsx
+++ components/bridge/Bridge.tsx
@@ -7,6 +7,7 @@ import TeamData from "./TeamData";
 import VioletButton from "components/VioletButton";
 import Person from "./Person";
 import Sponsors from "./SponsorData";
+import Intro from "./Intro";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -21,42 +22,7 @@ const Bridge: React.FC<{}> = () => {
   const classes = useStyles();
   return (
     <main id="main" className={classes.root}>
-      <Container component="section" id="intro">
-        <img
-          src={require("public/images/bridge/btytd-logo.svg")}
-          alt="Bridging the Youth Tech Divide Logo"
-        />
-        <Typography variant="h1">
-          Bridging the Youth Tech Divide 2020
-        </Typography>
-        <Typography variant="h2">
-          Gathering SF high school youth in unprecedented times to bridge the
-          tech divide.
-        </Typography>
-        <Typography variant="h2">
-          August 5th, 2020
-          <br />1 PM - 5 PM PDT
-        </Typography>
-        <Typography>
-          Bridging the Youth Tech Divide 2020 is a free conference led by SF
-          youth for SF youth, seeking to inspire attendees to explore the
-          potential and possibility of a tech career. We recognize the vast
-          disparities that prevent many youth from seeking out a tech career;
-          including, but not limited to, a lack of diversity, economic
-          inequality, and systemic racism. Attendees will be from
-          under-resourced and diverse backgrounds who demonstrate a compelling
-          interest in tech. The conference will feature speakers from Facebook
-          and Slack, relating their own stories and insights to youth interested
-          in breaking into tech. Bridging the Youth Tech Divide will also
-          feature a panel consisting of an array of professionals from all
-          corners of the tech industry, as well as a college student studying
-          Computer Science. Our goal is not that all attendees immediately set
-          out to pursue a tech career, but that they will leave inspired, having
-          a greater sense of what a tech career could mean for them. We hope to
-          learn from each other, support each other, and advocate for one
-          another to ensure a more equitable future in tech.
-        </Typography>
-      </Container>
+      <Intro />
       <Container component="section" id="register">
         <VioletButton
           href="https://www.tfaforms.com/4835468"
---
+++ components/bridge/BridgeDates.tsx
@@ -0,0 +1,19 @@
+import { CalendarEvent } from "vendor/calendar-link";
+import absoluteUrl from "src/absoluteUrl";
+import { hourStartEndParts } from "src/dates";
+
+export const BridgeCalendarEvent: CalendarEvent = {
+  title: "Bridging the Youth Tech Divide 2020",
+  start: "2020-08-05T13:00-07:00",
+  end: "2020-08-05T17:00-07:00",
+  location: "Online",
+  url: absoluteUrl("/bridge"),
+};
+
+export function bridgeStartEnd() {
+  const start = Date.parse(BridgeCalendarEvent.start);
+  const end = Date.parse(BridgeCalendarEvent.end);
+  return hourStartEndParts(start, end);
+}
+
+export default BridgeCalendarEvent;
