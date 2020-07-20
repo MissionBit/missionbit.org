3b8f332c55a2d3d34ca8b1b749cbab5556775f92
Bob Ippolito <bob@redivi.com>
2020-07-20 16:38:58 -0700

    WIP student speakers

--- components/demoday/StudentSpeakers.tsx
+++ components/demoday/StudentSpeakers.tsx
@@ -3,6 +3,12 @@ import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import CourseShowcases, {
+  CourseShowcaseProps,
+  ProjectProps,
+  StudentProps,
+} from "./StudentSpeakerData";
+import { brand } from "src/colors";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -14,7 +20,79 @@ const useStyles = makeStyles((theme) => ({
       fontSize: theme.typography.h3.fontSize,
     },
   },
+  showcase: {},
+  courseTitle: {
+    color: brand.indigo,
+  },
+  project: {},
+  projectTitle: {},
+  students: {},
+  student: {
+    margin: theme.spacing(0, 2),
+    "& > img": {
+      width: "100%",
+      height: "100%",
+      maxWidth: 150,
+      borderRadius: "50%",
+    },
+  },
 }));
+
+const Student: React.FC<StudentProps> = ({ name, image }) => {
+  const classes = useStyles();
+  return (
+    <picture className={classes.student}>
+      <source type="image/webp" srcSet={image.webp} />
+      <source type="image/jpeg" srcSet={image.srcSet} />
+      <img alt={name} src={image.jpg} />
+    </picture>
+  );
+};
+
+const Project: React.FC<ProjectProps> = ({
+  title,
+  students,
+  description,
+  href,
+}) => {
+  const classes = useStyles();
+  return (
+    <Box component="section" className={classes.project}>
+      <Typography variant="h4" className={classes.projectTitle}>
+        {title}
+      </Typography>
+      <Typography>{students.map((p) => p.name).join(", ")}</Typography>
+      <Typography>{description}</Typography>
+      <Typography>
+        <a href={href} target="_blank" rel="noopener noreferrer">
+          View Project
+        </a>
+      </Typography>
+      <Box className={classes.students}>
+        {students.map((props, i) => (
+          <Student key={i} {...props} />
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+const CourseShowcase: React.FC<CourseShowcaseProps> = ({
+  course,
+  projects,
+}) => {
+  const classes = useStyles();
+  return (
+    <Box component="section" className={classes.showcase}>
+      <Typography variant="h3" className={classes.courseTitle}>
+        {course.title}
+      </Typography>
+      {projects.map((props, i) => (
+        <Project {...props} key={i} />
+      ))}
+    </Box>
+  );
+};
 
 const StudentSpeakers: React.FC<{}> = () => {
   const classes = useStyles();
@@ -25,6 +103,9 @@ const StudentSpeakers: React.FC<{}> = () => {
           Student Speakers
         </Typography>
       </Container>
+      {CourseShowcases.map((props, i) => (
+        <CourseShowcase {...props} key={i} />
+      ))}
     </Box>
   );
 };
