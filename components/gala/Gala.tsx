bbe14c1a61eddd6c4fdf0c1cb5aa898fc3760db4
Bob Ippolito <bob@redivi.com>
2020-09-04 17:40:31 -0700

    sponsorship packages

--- components/gala/Gala.tsx
+++ components/gala/Gala.tsx
@@ -3,23 +3,21 @@ import AddToCalendar from "./AddToCalendar";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import GalaVideo from "./GalaVideo";
-import { Sponsors, SponsorLevel, SponsorLevels } from "./SponsorData";
 import { makeStyles } from "@material-ui/core/styles";
 import { GalaCalendarEvent, galaStartEnd } from "./GalaDates";
 import { DEVELOPMENT_EMAIL } from "src/emails";
-import Sponsor from "./Sponsor";
 import GalaLanding from "./GalaLanding";
 import FlourishSeparator from "components/programs/FlourishSeparator";
-
-function levelTitle(s: SponsorLevel): string {
-  return s.substring(0, 1).toUpperCase() + s.substring(1);
-}
-
-function pluralize<T>(n: number, singular: T, plural: T): T {
-  return n === 1 ? singular : plural;
-}
+import SponsorSection from "./SponsorSection";
+import Link from "@material-ui/core/Link";
+import { brand } from "src/colors";
+import IndigoButton from "components/IndigoButton";
+import { Box } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingBottom: theme.spacing(4),
+  },
   emojiBullets: {
     listStyle: "none",
     paddingLeft: 0,
@@ -36,32 +34,6 @@ const useStyles = makeStyles((theme) => ({
         paddingRight: "0.5em",
       },
     },
-  },
-  galaSponsors: {
-    display: "grid",
-    gridGap: theme.spacing(4),
-    gridTemplateColumns: "repeat(auto-fit, minmax(60px, 1fr))",
-    gridTemplateRows: "repeat(auto-fit, 60px)",
-    "& a": {
-      margin: 0,
-      alignSelf: "center",
-      height: "100%",
-    },
-    "& img": {
-      objectFit: "contain",
-      height: "100%",
-      width: "100%",
-    },
-  },
-  sponsorHeading: {
-    margin: theme.spacing(4, 0),
-    [theme.breakpoints.down("sm")]: {
-      fontSize: theme.typography.h5.fontSize,
-      fontWeight: theme.typography.h6.fontWeight,
-    },
-  },
-  sponsorSection: {
-    paddingBottom: theme.spacing(4),
   },
   copySection: {
     "& > p": {
@@ -84,13 +56,41 @@ const useStyles = makeStyles((theme) => ({
       fontWeight: theme.typography.h6.fontWeight,
     },
   },
+  videoSection: {
+    marginBottom: theme.spacing(8),
+  },
+  link: {
+    color: brand.violet,
+  },
+  button: {
+    fontSize: theme.typography.pxToRem(35),
+    [theme.breakpoints.down("sm")]: {
+      fontSize: theme.typography.pxToRem(18),
+    },
+  },
+  sponsorshipTitle: {
+    padding: theme.spacing(0, 6),
+    [theme.breakpoints.down("sm")]: {
+      padding: theme.spacing(0, 2),
+      fontSize: theme.typography.h5.fontSize,
+    },
+  },
+  sponsorshipCopy: {
+    fontSize: theme.typography.pxToRem(24),
+    padding: theme.spacing(4, 0),
+    [theme.breakpoints.down("sm")]: {
+      fontSize: theme.typography.pxToRem(16),
+      padding: theme.spacing(2, 0),
+    },
+  },
+  sponsorshipActions: {},
 }));
 
 const Gala: React.FC<{}> = () => {
   const classes = useStyles();
   const { date, time } = galaStartEnd();
   return (
-    <main id="main">
+    <main id="main" className={classes.root}>
       <GalaLanding />
       <FlourishSeparator />
       <Container component="section" className={classes.copySection}>
@@ -159,30 +159,45 @@ const Gala: React.FC<{}> = () => {
           </a>
           .
         </Typography>
+      </Container>
+      <FlourishSeparator />
+      <Container component="section" id="sponsorship-info">
+        <Typography
+          variant="h2"
+          className={classes.sponsorshipTitle}
+          align="center"
+        >
+          Looking to sponsor Mission Bit's Fourth Annual Gala?
+        </Typography>
+        <Typography className={classes.sponsorshipCopy} align="center">
+          Contact{" "}
+          <Link
+            className={classes.link}
+            href={`mailto:${DEVELOPMENT_EMAIL}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {DEVELOPMENT_EMAIL}
+          </Link>{" "}
+          with questions
+        </Typography>
+        <Box className={classes.sponsorshipActions} textAlign="center">
+          <IndigoButton
+            variant="outlined"
+            size="large"
+            href="/gala/sponsorship"
+            className={classes.button}
+          >
+            Gala Sponsorship Packages
+          </IndigoButton>
+        </Box>
+      </Container>
+      <FlourishSeparator />
+      <Container component="section">
         <GalaVideo />
       </Container>
-      <Container component="section" className={classes.sponsorSection}>
-        {SponsorLevels.map((level) => {
-          const sponsors = Sponsors.filter((props) => props.level === level);
-          return sponsors.length === 0 ? null : (
-            <React.Fragment key={level}>
-              <Typography
-                variant="h3"
-                align="center"
-                className={classes.sponsorHeading}
-              >
-                2019 {levelTitle(level)}{" "}
-                {pluralize(sponsors.length, "Sponsor", "Sponsors")}
-              </Typography>
-              <div className={classes.galaSponsors}>
-                {sponsors.map((props) => (
-                  <Sponsor key={props.href} {...props} />
-                ))}
-              </div>
-            </React.Fragment>
-          );
-        })}
-      </Container>
+      <FlourishSeparator />
+      <SponsorSection />
     </main>
   );
 };
