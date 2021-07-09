69ac84d4596327c83d22f8a888beca72a22423d3
Cora Monokandilos <cora.monokandilos@gmail.com>
2021-07-09 16:26:01 -0700

    new verizon logo

--- components/bridge/SponsorData.tsx
+++ components/bridge/SponsorData.tsx
@@ -68,46 +68,11 @@ export const Sponsors: SponsorData[] = [
 
 export const Employers: SponsorData[] = [
   {
-    href: "https://www.facebook.com/",
-    title: "Facebook",
-    width: 1600,
-    height: 900,
-    logoUrl: require("public/images/bridge/employers/facebook.svg"),
-  },
-  {
-    href: "https://slack.com/",
-    title: "Slack",
-    width: 622.3,
-    height: 254.4,
-    logoUrl: require("public/images/bridge/employers/slack.svg"),
-  },
-  {
-    href: "https://chanzuckerberg.com/",
-    title: "Chan Zuckerberg Initiative",
-    width: 112,
-    height: 62,
-    logoUrl: require("public/images/bridge/employers/chan-zuckerberg-initiative.svg"),
-  },
-  {
-    href: "https://www.getforefront.co/",
-    title: "Forefront",
-    width: 236,
-    height: 150,
-    logoUrl: require("public/images/bridge/employers/forefront.png"),
-  },
-  {
-    href: "https://discord.com/",
-    title: "Discord",
-    width: 800,
-    height: 272.1,
-    logoUrl: require("public/images/bridge/employers/discord.svg"),
-  },
-  {
-    href: "https://www.oculus.com/",
-    title: "Oculus",
-    width: 800,
-    height: 400,
-    logoUrl: require("public/images/bridge/employers/oculus.svg"),
+    href: "https://www.verizon.com/",
+    title: "Verizon",
+    width: 1262,
+    height: 258,
+    logoUrl: require("public/images/bridge/sponsors/verizon-big.png"),
   },
 ];
 
