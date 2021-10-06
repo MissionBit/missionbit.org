1b6be8796addf792c1e6d0a1bec3ddb9e6b7b39e
Bob Ippolito <bob@redivi.com>
2021-10-05 18:08:21 -0700

    Use next cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const { readFileSync } = require("fs");
 const withPlugins = require("next-compose-plugins");
-const withSourceMaps = require("@zeit/next-source-maps");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
@@ -37,4 +36,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPlugins([withBundleAnalyzer, withSourceMaps], nextConfig);
+module.exports = withPlugins([withBundleAnalyzer], nextConfig);
